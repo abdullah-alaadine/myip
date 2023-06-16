9a554e4969c052e54f7a615d613216f00d537bab
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-06-15 18:15:14 -0700

    install 'godotenv' to load env variables

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/abdullah-alaadine/RSS-Aggregator
 
 go 1.20
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -2,8 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	fmt.Println("hello world")
+
+	godotenv.Load(".env")
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT is not founded in the environment")
+	}
+
+	fmt.Println("PORT:", portString)
 }
