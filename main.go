8497c3af544fd90a6e68f680e14ec594d40a719f
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-11-29 20:31:35 +0200

    create getPrivateIP function

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import "net"
+
 func main() {
 
+}
+
+func getPrivateIP() (string, error) {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		return "", err
+	}
+	defer conn.Close()
+
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+
+	return localAddr.IP.String(), nil
 }
